d4bd27639d3953c236fd5967044c127933936140
Pablo Andres Gonzalez Casco <pablogon@gmail.com>
2016-08-09 13:54:44 -0300

    le agrego el gitignore

---
+++ EFDbFirst/EFDbFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EFDbFirst.Startup))]
+namespace EFDbFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
