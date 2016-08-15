14754be6630b5be78bcf401cb75be3bbd06b81a4
Pablo <pablogon@gmial.com>
2016-08-15 20:36:18 -0300

    tenia una linea de mas

--- RESTfulDay1/BusinessEntities/ProductEntity.cs
+++ RESTfulDay1/BusinessEntities/ProductEntity.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace BusinessEntities
 {
     public class ProductEntity
-    {C:\Users\pcasco\Documents\Visual Studio 2015\Projects\NetCoreApi\RESTfulDay1\BusinessEntities\ProductEntity.cs
+    {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
     }
