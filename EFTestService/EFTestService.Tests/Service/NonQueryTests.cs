7a3d3249458891b016b2b0c8dd6da40b442a5c4f
Pablo Andres Gonzalez Casco <pablogon@gmail.com>
2016-08-22 14:31:42 -0300

    anda la parte de testing

---
+++ EFTestService/EFTestService.Tests/Service/NonQueryTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data.Entity;
+using DataModel;
+
+namespace EFTestService.Tests
+{
+    [TestClass]
+    public class NonQueryTests
+    {
+        [TestMethod]
+        public void CreateBlog_saves_a_blog_via_context()
+        {
+            var mockSet = new Mock<DbSet<Blog>>();
+            var mockContext = new Mock<BloggingEntities>();
+            mockContext.Setup(m => m.Blog).Returns(mockSet.Object);
+
+            var service = new BlogService(mockContext.Object);
+            service.AddBlog("http://blogs.msdn.com/adonet");
+            mockSet.Verify(m => m.Add(It.IsAny<Blog>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+    }
+}
---
+++ EFTestService/EFTestService.Tests/Service/QueryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DataModel;
+using System.Linq;
+using System.Data.Entity;
+using Moq;
+
+namespace EFTestService.Tests.Service
+{
+    [TestClass]
+    public class QueryTests
+    {
+        [TestMethod]
+        public void GetAllBlogs_orders_by_name()
+        {
+            var data = new List<Blog>
+            {
+                new Blog { Url = "BBB" },
+                new Blog { Url = "ZZZ" },
+                new Blog { Url = "AAA" },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Blog>>();
+            mockSet.As<IQueryable<Blog>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Blog>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Blog>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Blog>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+
+            var mockContext = new Mock<BloggingEntities>();
+            mockContext.Setup(c => c.Blog).Returns(mockSet.Object);
+
+            var service = new BlogService(mockContext.Object);
+            var blogs = service.GetAllBlogs();
+
+            Assert.AreEqual(3, blogs.Count);
+            Assert.AreEqual("AAA", blogs[0].Url);
+            Assert.AreEqual("BBB", blogs[1].Url);
+            Assert.AreEqual("ZZZ", blogs[2].Url);
+
+        }
+    }
+}
